3d9e977c9fa9050a1ca268add52aba09b6de7554
Vladimir Ćirković <wladimir.cirkovic@gmail.com>
2025-09-16 21:51:23 +0200

    fix gh deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/',
-  server: {
-    historyApiFallback: true,
-  },
   build: {
     rollupOptions: {
       output: {
